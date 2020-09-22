fadf7a934b8cddafa9d1bc8d5838423545cf4f42
0x00stone <0x00stone@gmail.com>
2020-09-22 11:59:17 +0800

    kidsWithCandies1431

---
+++ src/leetcode/array/kidsWithCandies1431/Solution.java
@@ -0,0 +1,18 @@
+package leetcode.array.kidsWithCandies1431;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution{
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max = 0;
+        for(int i : candies ){
+            max = i > max ? i :max ;
+        }
+        ArrayList< Boolean> list = new ArrayList<Boolean>();
+        for(int j =0 ; j<= candies.length-1 ; j++){
+            list.add(candies[j] + extraCandies >= max ? true :false);
+        }
+        return list;
+    }
+}
