17086db79957214d98677459454d0f3e23e08502
0x00stone <0x00stone@gmail.com>
2020-09-22 19:14:53 +0800

    createtargetArray1389

--- src/leetcode/array/createTargetArray1389/Solution.java
+++ src/leetcode/array/createTargetArray1389/Solution.java
@@ -1,4 +1,19 @@
 package leetcode.array.createTargetArray1389;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Solution {
+    public int[] createTargetArray(int[] nums, int[] index) {
+        List list = new ArrayList();
+        int[] ans = new int[index.length];
+        for(int i = 0; i < index.length ; i++){
+            list.add(index[i], nums[i]);
+        }
+        for(int j = 0; j < index.length ; j++){
+            ans[j] = (int) list.get(j);
+        }
+        return ans;
+    }
 }
