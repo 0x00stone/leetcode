d4ae3866563cc4c7b05cee6f16c899d8d2f0d548
0x00stone <0x00stone@gmail.com>
2020-09-22 23:03:02 +0800

    decompressRLElist131

---
+++ src/leetcode/array/decompressRLElist1313/Solution.java
@@ -0,0 +1,4 @@
+package leetcode.array.decompressRLElist1313;
+
+public class Solution {
+}
---
+++ src/leetcode/array/decompressRLElist1313/demo.java
@@ -0,0 +1,10 @@
+package leetcode.array.decompressRLElist1313;
+
+public class demo {
+    public static void main(String[] args) {
+        int[] nums1 = {1,2,3,4};//[2,4,4,4]
+        int[] nums2 = {1,1,2,3};//[1,3,3]
+
+        Solution s = new Solution();
+    }
+}
