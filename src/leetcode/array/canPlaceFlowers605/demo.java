9689109b66995dcebba9402f7c88033777f0a3c5
0x00stone <0x00stone@gmail.com>
2020-09-23 21:26:36 +0800

    605

---
+++ src/leetcode/array/canPlaceFlowers605/demo.java
@@ -0,0 +1,15 @@
+package leetcode.array.canPlaceFlowers605;
+
+public class demo {
+    public static void main(String[] args) {
+        int[] flowerbed1 = {1,0,0,0,1};
+        int n1 = 1 ;//True
+
+        int[] flowerbed2 = {1,0,0,0,1};
+        int n2 = 2;//False
+
+        Solution s = new Solution();
+        System.out.println(s.canPlaceFlowers(flowerbed1,n1));
+        System.out.println(s.canPlaceFlowers(flowerbed2,n2));
+    }
+}
