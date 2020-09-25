6349d1c98025d8de3084174a15dfe1cdcf1ca4cc
0x00stone <0x00stone@gmail.com>
2020-09-25 23:10:07 +0800

    findUnsortedSubarray581

--- src/leetcode/array/findUnsortedSubarray581/demo.java
+++ src/leetcode/array/findUnsortedSubarray581/demo.java
@@ -2,17 +2,18 @@ package leetcode.array.findUnsortedSubarray581;
 
 public class demo {
     public static void main(String[] args) {
-        int[] arr4 = {1,2,0,3,4,5,6,7,8,9};//3
-        int[] arr0 = {1,2,3,3,3};//0
-        int[] arr1 =  {2, 6, 4, 8, 10, 9, 15};//5
-        int[] arr2 = {1,2,3,4};//0
-        int[] arr3 = {2,1};//0
+        int[] arr4 = {1,2,0,3,4,5,6,7,8,9};
+        int[] arr0 = {1,2,3,3,3};
+        int[] arr1 =  {2, 6, 4, 8, 10, 9, 15};
+        int[] arr2 = {1,2,3,4};
+        int[] arr3 = {2,1};
 
-        Solution s= new Solution();
-        System.out.println(s.findUnsortedSubarray(arr4));
-        System.out.println(s.findUnsortedSubarray(arr3));
-        System.out.println(s.findUnsortedSubarray(arr0));
-        System.out.println(s.findUnsortedSubarray(arr1));
-        System.out.println(s.findUnsortedSubarray(arr2));
+        Solution2 s= new Solution2();
+        System.out.println(s.findUnsortedSubarray(arr3));//2
+        System.out.println(s.findUnsortedSubarray(arr4));//3
+
+        System.out.println(s.findUnsortedSubarray(arr0));//0
+        System.out.println(s.findUnsortedSubarray(arr1));//5
+        System.out.println(s.findUnsortedSubarray(arr2));//0
     }
 }
